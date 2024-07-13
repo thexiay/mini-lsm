a5a9e62cefcf89b5d110da6c4009c2ca6bf18be6
thexia <xiayu1187@gmail.com>
2024-07-13 16:38:22 +0800

    [week2day1] task2

--- mini-lsm-starter/src/iterators/concat_iterator.rs
+++ mini-lsm-starter/src/iterators/concat_iterator.rs
@@ -4,10 +4,12 @@
 use std::sync::Arc;
 
 use anyhow::Result;
+use bytes::Bytes;
+use log::debug;
 
 use super::StorageIterator;
 use crate::{
-    key::KeySlice,
+    key::{KeyBytes, KeySlice},
     table::{SsTable, SsTableIterator},
 };
 
@@ -15,17 +17,48 @@ use crate::{
 /// iterators when initializing this iterator to reduce the overhead of seeking.
 pub struct SstConcatIterator {
     current: Option<SsTableIterator>,
-    next_sst_idx: usize,
+    current_sst_idx: usize,
     sstables: Vec<Arc<SsTable>>,
 }
 
 impl SstConcatIterator {
     pub fn create_and_seek_to_first(sstables: Vec<Arc<SsTable>>) -> Result<Self> {
-        unimplemented!()
+        let current = sstables
+            .first()
+            .map(|sst| SsTableIterator::create_and_seek_to_first(sst.clone()))
+            .transpose()?;
+        let current_sst_idx = 0;
+        Ok(Self {
+            current,
+            current_sst_idx,
+            sstables,
+        })
     }
 
     pub fn create_and_seek_to_key(sstables: Vec<Arc<SsTable>>, key: KeySlice) -> Result<Self> {
-        unimplemented!()
+        // todo: 二分查找来优化
+        let key = {
+            sstables.first().map_or(KeySlice::default(), |sst| {
+                let first_key = sst.first_key();
+                if key < first_key.as_key_slice() {
+                    first_key.as_key_slice()
+                } else {
+                    key
+                }
+            })
+        };
+        let item = sstables.iter().enumerate().find(|(_, sst)| 
+            sst.first_key().as_key_slice() <= key && key <= sst.last_key().as_key_slice()
+        );
+        let current = item
+            .map(|(_, sst)| SsTableIterator::create_and_seek_to_key(sst.clone(), key))
+            .transpose()?;
+        let current_sst_idx = item.map(|(idx, _)| idx).unwrap_or(0);
+        Ok(Self {
+            current,
+            current_sst_idx,
+            sstables,
+        })
     }
 }
 
@@ -33,22 +66,41 @@ impl StorageIterator for SstConcatIterator {
     type KeyType<'a> = KeySlice<'a>;
 
     fn key(&self) -> KeySlice {
-        unimplemented!()
+        self.current
+            .as_ref()
+            .map_or(KeySlice::default(), |iter| iter.key())
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        self.current.as_ref().map_or(&[], |iter| iter.value())
     }
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        self.current.as_ref().map_or(false, |iter| iter.is_valid())
     }
 
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        let iter = match self.current.as_mut() {
+            Some(iter) => iter,
+            None => return Ok(()),
+        };
+
+        iter.next()?;
+        if !iter.is_valid() {
+            self.current_sst_idx += 1;
+            if self.current_sst_idx < self.sstables.len() {
+                self.current = Some(SsTableIterator::create_and_seek_to_first(
+                    self.sstables[self.current_sst_idx].clone(),
+                )?);
+            } else {
+                self.current = None;
+            }
+        }
+
+        Ok(())
     }
 
     fn num_active_iterators(&self) -> usize {
-        1
+        self.sstables.len()
     }
 }
